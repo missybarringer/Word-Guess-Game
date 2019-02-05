8febff2b87355d713b3051adfb8919a29c00769c
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-02-05 16:01:10 -0600

    updated pseudocode & readme

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// Pseudocode
+// Main header is static
+// Song guessed correctly prints when won and play the audio as well
+// Print text saying "Press any Key to get started!"
+// Next print wins and increment per win
+// Print current word then dashes below that correspond to the length of the word 
+// (letters appear here until word is guessed or # of guesses remaining are out) what happens at that point???
+// If guessed print word & increment win counter
+// Number of guesses remaining (start at 12 or 13 and decrement with each wrong guess)
+// Letters Already Guessed
+// Print letters typed (if same letter typed previously doesn't print here and doesn't decrease remaining)
+// Left column has the picture of the band that they one with - changes with next win
