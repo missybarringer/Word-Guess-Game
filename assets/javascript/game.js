8adc12cbe27610bf86201aca463eb8f85c076664
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-02-06 20:48:51 -0600

    updated code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,3 +10,60 @@
 // Letters Already Guessed
 // Print letters typed (if same letter typed previously doesn't print here and doesn't decrease remaining)
 // Left column has the picture of the band that they one with - changes with next win
+
+var wins = 0;
+//Array of Animals
+var farmAnimals = ["donkey","horse","alpaca","chicken","goat"];
+//Choose a random word
+var randNum = Math.floor(Math.random() * farmAnimals.length);
+var rightWord = [];
+var wrongWord = [];
+var choosenWord = farmAnimals[randNum];
+var underScore = [];
+
+let docUnderScore = document.getElementById(underScore);
+//Create underscores based on the length of the word
+
+function generateUnderscore() {
+    for(var i = 0; i < choosenWord.length; i++) {
+        underScore.push('_');
+        
+    }
+    return underScore;
+// document.getElementById("generateUnderscore").innerHTML=generateUnderscore;
+}
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;   //51:41
+// if Users guess is right
+    if(choosenWord.indexOf(userGuess) > -1) {
+        //if Users guess is right
+        rightWord.push(userGuess);
+        underScore[choosenWord.indexOf(userGuess)] = userGuess;
+        // docUnderScore[0].innerHTML = underScore.join(' ');
+        // docRightGuess[0].innerHTML = rightWord;
+        // if(underScore.join('') == choosenWord) {
+        //     alert('You Win');
+        // }
+        console.log(rightWord);
+    }
+    else {
+        wrongWord.push(userGuess);
+        console.log(wrongWord);
+    console.log(userGuess);
+    console.log(underScore);
+    }
+    // document.getElementById(userGuess).innerHTML = userGuess;
+};
+
+// document.getElementById("generateUnderscore").innerHTML="Yes";
+window.onload = function() {
+    score();
+    function score() {
+        document.getElementById("wins").innerHTML =wins;
+    };
+}
+
+
+console.log(choosenWord);
+console.log(generateUnderscore());
